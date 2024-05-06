e856e046cd6e8a41cceae07268fda6d80f91d30e
cdytoby <cdytoby@live.cn>
2024-05-06 20:59:04 +0200

    Reproduce bug

--- AvaloniaBugReport.Core/ViewModels/MainWindowViewModel.cs
+++ AvaloniaBugReport.Core/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ObservableCollections;
 
 namespace AvaloniaBugReport.Core.ViewModels;
 
@@ -7,16 +11,46 @@ public partial class MainWindowViewModel: ObservableObject
     [ObservableProperty]
     private int testInt = 5;
     
+    [ObservableProperty]
+    private INotifyCollectionChangedSynchronizedView<string> testNotifiedEnumerable;
+    
+    [ObservableProperty]
+    private ObservableCollection<string> testCollection = [];
+    
     public IUIThreadExecutor? uiExecutor;
+    
+    private ObservableList<string> testList = [];
     
     public MainWindowViewModel()
     {
+        TestNotifiedEnumerable = testList.CreateView(x => x).ToNotifyCollectionChanged();
+        TestNotifiedEnumerable.CollectionChanged += TestNotifiedEnumerableOnCollectionChanged;
+        
+        testCollection.Add("Should Work 1");
+        testCollection.Add("Should Work 2");
+        
         Task calculateTask = Calculate();
+    }
+    
+    private void TestNotifiedEnumerableOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        Debug.WriteLine("TestNotifiedEnumerableOnCollectionChanged called");
     }
     
     private async Task Calculate()
     {
         await Task.Delay(1000);
         uiExecutor?.Call(() => TestInt = 10);
+        uiExecutor?.Call(() =>
+        {
+            TestCollection.Add("Should Work 3");
+            TestCollection.Add("Should Work 4");
+        });
+        
+        uiExecutor?.Call(() =>
+        {
+            testList.Add("ListItem1");
+            testList.Add("ListItem2");
+        });
     }
 }
